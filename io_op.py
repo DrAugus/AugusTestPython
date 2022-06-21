b45cb0ba8d1b0acb70b610fab51a5992b91b92f9
Augus <bramlye@outlook.com>
2022-06-22 00:01:15 +0800

    feat: io op

---
+++ io_op.py
@@ -0,0 +1,22 @@
+def io_op():
+    f = open('./data/test.txt', 'a+', encoding='utf-8')
+    data = f.write('\n' 'g234')
+    f.seek(0)
+    read1 = f.read()
+    print(read1)
+    f.close()
+
+
+def one_by_one():
+    f = open('./data/test.txt', encoding='utf-8')
+    line = f.readline()
+    while line:
+        s = line.__str__().replace('\n', '')
+        print('[' + s + '](./' + s + '.md)')
+        line = f.readline()
+    f.close()
+
+
+if __name__ == '__main__':
+    # io_op()
+    one_by_one()
